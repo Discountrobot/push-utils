fbd858091b0ee18bceb8fae5b9f1b6fb862f05f4
discountrobot <discountrobot@gmail.com>
2017-08-06 14:38:10 +0200

    refactor(examples): Update browser example to use typescript

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -1,23 +1,26 @@
-var debug = process.env.NODE_ENV !== "production";
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  context: __dirname,
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./scripts.js",
+  entry: path.resolve(__dirname, 'scripts.ts'),
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    ],
+  },
   output: {
     path: __dirname,
     filename: "scripts.min.js"
   },
   resolve: {
     alias: {
-      'node-forge': path.resolve(__dirname, 'forge.min.js')
+      'node-forge': path.resolve(__dirname, 'vendor', 'forge.min.js')
     }
-  },
-  plugins: debug ? [] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ]
+  }
 };
